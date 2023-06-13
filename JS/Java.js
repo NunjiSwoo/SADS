712c0fd54eeedd8bca76073a2fc34f2f1082c3e1
NunjiSwoo <renanoliveira30@hotmail.com>
2023-06-12 21:02:35 -0300

    video

--- JS/Java.js
+++ JS/Java.js
@@ -20,3 +20,25 @@ function nextImage(){
     }
 
 }
+
+var videos = document.querySelectorAll("#videos");
+
+    function playVideo() {
+      this.play();
+    }
+
+    function pauseVideo() {
+      this.pause();
+    }
+
+    function resetVideo() {
+      this.currentTime = 0;
+    }
+
+    videos.forEach(function(video) {
+      video.addEventListener("mouseover", playVideo);
+      video.addEventListener("mouseout", function() {
+        pauseVideo.call(video);
+        resetVideo.call(video);
+      });
+    });
